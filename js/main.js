11ffedbe8b41b4e3b1346d9862b97635d661b7a4
Dana Marín García <dana.mg@MacBook-Air-de-Dana.local>
2019-06-03 23:42:54 -0400

    first try with JS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 (() => {
   //sub
-  console.log('fired!');
+console.log('fired!');
 
   //CONSTANTS:
 
-  
+const cassettesPieces = document.querySelector(".draggableIcons"),
+		dropZone = document.querySelector(".musicMixer");
+		
+	
+
+let draggablePieces = cassettesPieces.querySelectorAll("img");
+
+draggablePieces.forEach(piece => {
+	piece.addEventListener("dragstart", function(e) {
+		console.log('draggin...');
+
+			e.dataTransfer.setData("text/plain", this.id);
+		});
+	});
+// HASTA AQUÍ TODO IBA BIEN...
+
+dropZone.forEach(zone => {
+	//allow user to drag over an element
+	zone.addEventListener("dragover", function(e) {
+		e.preventDefault();
+		console.log("dragged sumpin over me");
+});
+
+zone.addEventListener("drop", function(e) {
+	e.preventDefault();
+	console.log("you dropped sumpin on me");
+
+	let draggedElement = e.dataTransfer.getData("text/plain");
+	console.log('you dragged: ', draggedElement);
+
+	//add the image to the drop zone
+	e.target.appendChild(document.querySelector(`#${draggedElement}`));
+	});
+
+})();
